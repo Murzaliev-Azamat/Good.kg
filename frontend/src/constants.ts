ed36aa8552a615d51623848fc99d330e08712b3e
azamatmurzaliev <Lirog.kg@gmail.com>
2023-11-05 19:48:18 +0600

    Frontend:
    Change http://localhost:8000 on http://104.248.43.43:8000 in file .env,local

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,5 @@
+export const apiUrl = process.env.REACT_APP_API_URL;
+export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
 // export const apiUrl = 'http://api.adilet.club';
 // export const apiUrl = 'http://localhost:8000';
 // export const GOOGLE_CLIENT_ID = '1033022370181-dphif33ggrmhgs75vfqp559pec80vthv.apps.googleusercontent.com';
-export const apiUrl = process.env.REACT_APP_API_URL;
-export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
