ba85449fe17c8b65667946811670969060282920
azamatmurzaliev <Lirog.kg@gmail.com>
2023-11-21 04:03:39 +0600

    Frontend: работа над исправлением отображения на iphone backgroundAttachment: 'fixed'

--- frontend/src/components/UI/AdvBlock/AdvBlock.tsx
+++ frontend/src/components/UI/AdvBlock/AdvBlock.tsx
@@ -6,19 +6,42 @@ interface Props extends React.PropsWithChildren {
 }
 
 const AdvBlock: React.FC<Props> = ({ urlImage, children }) => {
+  // const MyBlock = styled('div')({
+  //   height: '100%',
+  //   backgroundImage: 'url(' + urlImage + ')',
+  //   backgroundPosition: '50% 63px',
+  //   backgroundSize: 'cover',
+  //   backgroundAttachment: 'fixed',
+  //   paddingTop: 230,
+  // });
   const MyBlock = styled('div')({
     height: '100%',
-    backgroundImage: 'url(' + urlImage + ')',
-    // backgroundPosition: "center",
-    backgroundPosition: '50% 63px',
-    backgroundSize: 'cover',
-    backgroundAttachment: 'fixed',
+    position: 'relative',
+    overflow: 'hidden',
     paddingTop: 230,
+    '&:before': {
+      content: '""',
+      position: 'fixed',
+      display: 'block',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      backgroundSize: 'cover',
+      backgroundPosition: '50% 63px',
+      backgroundImage: `url(${urlImage})`,
+      transform: 'translateZ(0)',
+      willChange: 'transform',
+      zIndex: -1,
+    },
   });
 
   return (
     <MyBlock>
-      <div style={{ maxWidth: '1200px', margin: '0 auto', overflow: 'hidden', minHeight: '100vh' }}>{children}</div>
+      {/*<div style={{ maxWidth: '1200px', margin: '0 auto', overflow: 'hidden', minHeight: '100vh' }}>{children}</div>*/}
+      <div style={{ maxWidth: '1200px', margin: '0 auto', minHeight: '100vh', position: 'relative', zIndex: 1 }}>
+        {children}
+      </div>
     </MyBlock>
   );
 };
